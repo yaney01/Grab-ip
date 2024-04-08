8866101ce6e2ac31371da6537a54dadee1580b3e
yaney <53202722+yaney01@users.noreply.github.com>
2024-04-08 13:47:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,5 +76,5 @@ def convert_ips(input_urls, output_files):
 
 if __name__ == "__main__":
     input_urls = ["https://ipdb.api.030101.xyz/?type=bestproxy", "https://ipdb.api.030101.xyz/?type=bestcf", 'https://raw.githubusercontent.com/China-xb/zidonghuaip/main/ip.txt', 'https://addressesapi.090227.xyz/CloudFlareYes' , 'https://kzip.pages.dev/a.csv?token=mimausb8' , 'https://cfno1.pages.dev/pure']  # 包含IP地址的txt文件的多个URL
-    output_files = ["bestproxy.txt", "bestcf.txt", 'ip.txt', 'cfip.txt', 'kzip.txt']
+    output_files = ['ip.txt']
     convert_ips(input_urls, output_files)
